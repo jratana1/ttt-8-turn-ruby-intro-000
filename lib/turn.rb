88fcad327b0cf5827851bd796b31f902fd42f8ce
John Ratanaprasatporn <jratana1@gmail.com>
2020-09-03 02:27:55 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,6 +38,7 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index) == true
     move(board, index, character = "X")
+    puts display_board(board)
   else
     turn(board)
   end
